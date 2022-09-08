5f0ed7563c9c3cc128c0c8bf88f6cfdc6e1f6e0f
johnwalked <yohannesbelay86@gmail.com>
2022-09-08 16:14:09 -0700

    removed some spaces beacause betty jst don't like um

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
 
-
-
 /**
- *
- *  *main - Entry point
- *
- *   *Return: Always 0 (Success)
- *
- *    */
-
-
+ * main - Entry point
+ * Description: A C program that prints a sentence
+ * Return: Always 0 (Success)
+*/
 
 int main(void)
 
