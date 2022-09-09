44adf934e95c7a1006dbf0bd02555c4fac2e119f
johnwalked <yohannesbelay86@gmail.com>
2022-09-09 10:02:54 -0700

    fixed betty style

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,24 +1,15 @@
 #include <stdio.h>
 
-
-
 /**
-
  *main - print lowercase alpha a-z but remove 'q' and 'e'
-
  *Return: Always 0 (Success)
-
- */
-
-
+*/
 
 int main(void)
 
 {
 
 	char alpha = 'a';
-
-
 
 	while (alpha <= 'z')
 
@@ -37,9 +28,7 @@ int main(void)
 	}
 
 	putchar('\n');
-
-
-
+	
 	return (0);
 
 }
