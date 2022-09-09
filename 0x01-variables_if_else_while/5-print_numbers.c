0589362428c6df6906b63980aa52f30b4538b7f8
johnwalked <yohannesbelay86@gmail.com>
2022-09-09 09:59:46 -0700

    intial

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ *main - print from 0-9
+ *Return: Always 0 (Success)
+*/
+
+int main(void)
+
+{
+
+	int n = 0;
+
+	while (n < 10)
+
+	{
+
+		printf("%i", n);
+
+		n++;
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ *main - print reversed alphabet using putchar
+ *Return: Always 0 (Success)
+*/
+
+int main(void)
+
+{
+
+	char alpha = 'z';
+
+	while (alpha >= 'a')
+
+	{
+
+		putchar(alpha);
+
+		alpha--;
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
