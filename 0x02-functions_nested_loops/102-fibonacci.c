4e5cf6db5c074ef57400485263d2c1e81b3311de
johnwalked <yohannesbelay86@gmail.com>
2022-09-13 23:53:51 +0000

    fix

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,21 +7,32 @@
 
 int main(void)
 {
-	int counter;
-	int countto = 50;
-	long a = 1;
-	long b = 2;
+int counter;
 
-	for (counter = 1; counter <= (countto / 2); counter++)
-	{
-		printf("%li, %li, ", a, b);
-		a += b;
-		b += a;
-	}
-	if (countto % 2 == 1)
-		printf("%li", a);
+int countto = 50;
 
-	printf("\n");
+long a = 1;
 
-	return (0);
+long b = 2;
+
+for (counter = 1; counter <= (countto / 2); counter++)
+{
+if (counter == 25)
+{
+printf("%li, %li ", a, b);
+
+}
+else if ((counter != 25))
+printf("%li, %li, ", a, b);
+
+a += b;
+
+b += a;
+
+}
+
+printf("\n");
+
+return (0);
+
 }
