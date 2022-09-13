1015c125fb9dddb70518a4d5bf27016edfd9c0b3
johnwalked <yohannesbelay86@gmail.com>
2022-09-13 22:51:29 +0000

    fix3

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,11 +2,12 @@
 
 /**
 * print_last_digit - print last digit of integer
-* n: integer argument
+* @n: integer argument
 * Return: last digit of number
 */
 
 int print_last_digit(int n)
+
 {
 	if (n < 0)
 	n *= -1;
