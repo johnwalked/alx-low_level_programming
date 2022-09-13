72b40128ec17adea5245d2ae490670ab76fd294f
johnwalked <yohannesbelay86@gmail.com>
2022-09-13 22:07:23 +0000

    commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,21 +8,16 @@
 void print_alphabet_x10(void)
 {
 	char lower;
+	int i;
 
-	lower = 'a';
-
-int i = 0;
-
-for (i < 10; i++)
-
-{
-
-	while (lower <= 'z')
+	for (i = 1; i <= 10; i++)
 	{
-		_putchar(lower);
-		lower++;
+		for (lower = 'a'; lower <= 'z'; lower++)
+		{
+			_putchar(lower);
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
-}
+return (0);
 
 }
