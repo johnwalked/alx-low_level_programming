dfeebbdb1bf7080f27d5e846b89a15e2912e6a7f
johnwalked <yohannesbelay86@gmail.com>
2022-09-15 21:21:41 +0000

    intial commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+ * main - a function that checks for uppercase character
+ * Return: Always 0
+ */
+
+int _isupper(int c)
+{
+	char upper;
+
+	if (upper > 'A' && upper < 'Z')
+
+		return (1);
+
+	else
+
+		return (0);
+}
