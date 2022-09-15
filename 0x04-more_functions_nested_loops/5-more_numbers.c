b43d0b7169aa7b5fa65bea1a1d56bc167e64a29f
johnwalked <yohannesbelay86@gmail.com>
2022-09-15 23:09:14 +0000

    commit

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * void more_numbers - prints 10 times the numbers, from 0 to 14
+ */
+
+void more_numbers(void)
+
+{
+	char i;
+	int counter;
+
+	for (counter = 1; counter <= 10; counter++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i / 10 > 0)
+			{
+				putchar('1');
+			}
+		putchar((i % 10) + '0');
+
+		}
+		 putchar('\n');
+	}
+}
