cd63acfbff104f1b22247951598349ba53c356f7
johnwalked <yohannesbelay86@gmail.com>
2022-09-15 22:39:06 +0000

    commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,13 +8,13 @@ void print_most_numbers(void);
 
 {
 
-	int i;
+	char i;
 
-	for (i = 0; i < 10; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		if (!(i == 2 || i == 4))
+		if (!(i == '2' || i == '4'))
 		{
-			_putchar (i + 48);
+			_putchar (i);
 		}
 	}
 	_putchar ('\n');
