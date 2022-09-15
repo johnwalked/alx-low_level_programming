f2dc38864fd3b921faa0736170cbfa52c9aa8cc4
johnwalked <yohannesbelay86@gmail.com>
2022-09-15 22:03:24 +0000

    commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - A function that mutiplies two integers
+ * @a: 1st number
+ * @b: 2nd number
+ * Return: the product of a and b.
+ */
+
+int mul(int a, int b)
+
+{
+	return(a * b);
+
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - A function that prints 0 to 9
+ * Return: 0
+ */
+
+void print_numbers(void)
+
+{
+
+	for (int i = 0; i <= 9; i++)
+
+	printf ("%d", i);
+
+}
