09d0cc7dbc5001e3e90e914362108d8195a48e6d
johnwalked <yohannesbelay86@gmail.com>
2022-09-22 14:28:39 -0700

    modified script

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,56 +1,50 @@
 #include "main.h"
 #include <stdio.h>
-/**
- * infinite_add - multiplies two digits
- * @n1: 1st digit in the multiplication
- * @n2: 1st digit in the multiplication
- * @r: 1st digit in the multiplication
- * @size_r: 1st digit in the multiplication
- * Description: multiplies two digits
- * Return: the result of the multiplication
- */
-char *infinite_add(char *n1, char *n2,
-		char *r, int size_r)
-{
-	int fnum, snum, sum, base, rem = 0, n1length = 0, n2length = 0;
 
-	while (n1[++n1length] != '\0')
-	n1length--;
-	while (n2[++n2length] != '\0')
-	n2length--;
-	size_r--;
-	while (n1length >= 0 || n2length >= 0)
+/**
+ * infinite_add - adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r: buffer for result
+ * @size_r: buffer size
+ * ahhh! Crazy task! Expand your knowledge
+ * Return: address of r or 0
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	{
-		if (size_r < 0)
-			return (0);
-		if (n1length >= 0)
-			fnum = n1[n1length] - '0';
-		else
-			fnum = 0;
-		if (n2length >= 0)
-			snum = n2[n2length] - '0';
-		else
-			snum = 0;
-		sum = fnum + snum + rem;
-		if (sum > 9)
-		{
-			base = sum % 10;
-			r[size_r] = base + '0';
-			rem = sum / 10;
-		}
-		else
-		{
-			base = sum;
-			r[size_r] = base + '0';
-			rem = 0;
-		}
-		size_r--;
-		n1length--;
-		n2length--;
-	}
-	if (size_r < 0 && rem)
+	int i, j, k, l, m, n;
+
+	for (i = 0; n1[i]; i++)
+		;
+	for (j = 0; n2[j]; j++)
+		;
+	if (i > size_r || j > size_r)
 		return (0);
-	else if (size_r >= 0 && rem)
-		r[size_r] = rem;
+		m = 0;
+	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
+	{
+		n = m;
+	if (i >= 0)
+		n += n1[i] - '0';
+	if (j >= 0)
+		n += n2[j] - '0';
+	if (i < 0 && j < 0 && n == 0)
+	{
+		break;
+	}
+		m = n / 10;
+		r[k] = n % 10 + '0';
+	}
+		r[k] = '\0';
+	if (i >= 0  j >= 0  m)
+		return (0);
+	for (k -= 1, l = 0; l < k; k--, l++)
+	{
+		m = r[k];
+		r[k] = r[l];
+		r[l] = m;
+
+	}
 	return (r);
 }
